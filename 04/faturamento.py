181dc91a19a3e5568fccab34218bd3192b8f75e2
Gabriel Soares <gabrsoares@outlook.com>
2024-09-03 18:57:49 -0300

    add code

---
+++ 04/faturamento.py
@@ -0,0 +1,17 @@
+valores = {
+    'SP':67836.43,
+    'RJ':36678.66,
+    'MG':29229.88,
+    'ES':27165.48,
+    'OUTROS':19849.53 
+}
+
+total = 0
+for valor in valores.values():
+    total += valor
+
+print('Percentual de representação')
+
+for estado, valor in valores.items():
+    percentual = (valor * 100.0)/ total
+    print(f'{estado} - {percentual:.2f}%')
